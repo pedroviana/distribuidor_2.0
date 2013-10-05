67ed734598b0cf911192eaeacbc2e490d6238159
iMac VM Ubuntu Ruby 2.0 <pedro.viana.pav@gmail.com>
2013-10-05 00:37:16 -0300

    teste gmail

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -72,7 +72,7 @@ Api32::Application.configure do
   config.action_mailer.default_url_options = { :host => 'ford.dnadigital.mobi' }
 
   # LOCAWEB SETTINGS
-  
+=begin
   config.action_mailer.smtp_settings = {
     :address => "smtp.fordeconoshow.com.br",
     :port => 587,
@@ -81,6 +81,16 @@ Api32::Application.configure do
     :user_name => "contato@fordeconoshow.com.br",
     :password => "Dna32979391"
   }
+=end
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'dnacom.com.br',
+    user_name:            'developer@dnacom.com.br',
+    password:             'ap050604',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
