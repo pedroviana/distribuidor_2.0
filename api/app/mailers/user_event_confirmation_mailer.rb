8797dd85ded301a75be684519e08f176074f3857
iMac VM Ubuntu Ruby 2.0 <pedro.viana.pav@gmail.com>
2013-10-09 01:04:54 -0300

    pdf attachment mailer

--- api/app/mailers/user_event_confirmation_mailer.rb
+++ api/app/mailers/user_event_confirmation_mailer.rb
@@ -7,10 +7,19 @@ class UserEventConfirmationMailer < ActionMailer::Base
   #helper :application # gives access to all helpers defined within `application_helper`.
 
   def send_qr( record )
-    attachments["convite.jpeg"] = File.read(record.qr_path)
 	  @record = record
+    @image      = "#{AppSettings.public_url}/header_pdf.png"
+    @qr_code    = "#{AppSettings.public_url}/qrcodes/#{record.qr_path.split('/').last}"
+    @latitude   = record.event.latitude
+    @longitude  = record.event.longitude
+    
+    partial = render_to_string(:partial => 'invite', :layout => false)
+
+    attachments["convite.pdf"] = PDFKit.new(partial).to_pdf
+
     @logo_image = "#{AppSettings.public_url}/logo_ford_mail.jpg"
     @line_image = "#{AppSettings.public_url}/barra_mail.jpg"
+
 	 	mail(:to => record.user.email, :subject => "Sua presença no #{record.event.title} foi confirmada!")
 	end
 end
