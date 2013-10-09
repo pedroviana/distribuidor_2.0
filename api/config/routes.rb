4d4914b36ed6a42cb6db03d4c1de05d801847399
iMac VM Ubuntu Ruby 2.0 <pedro.viana.pav@gmail.com>
2013-10-09 09:57:58 -0300

    removendo rotas nao utilizadas

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,10 +4,6 @@ Api32::Application.routes.draw do
 
   root to: "admin/dashboard#index"
   get "qr/index"
-  
-#  get "user_events/show"
-#  get "user_events/invalid_token"
-#  post "user_events/update"
 
   resources :user_event_confirmations do
     collection do
